11195ecc390dd742a565a1eb4e8fc22a4e7a0026
Greg Banks <gregbanks@Gregs-MacBook-Pro.local>
2022-01-03 02:43:40 -0600

    Added constants and client routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import Header from './components/Header/Header';
+import React from "react";
+import Header from "./components/Header/Header";
+import SignUp from "./components/SignUp/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <SignUp />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -1,57 +1,106 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../../constants/apiConstants";
 
 export default function SignUp(props) {
-    const [user , setUser] = useState({
-        email : "",
-        password : ""
-    })
-    const handleChange = (e) => {
-        const {id , value} = e.target   
-        setUser(prevState => ({
-            ...prevState,
-            [id] : value
-        }))
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setUser((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+  const sendDetailsToServer = () => {
+    if (user.email.length && user.password.length) {
+      props.showError(null);
+      const payload = {
+        email: user.email,
+        password: user.password,
+      };
+      axios
+        .post(API_BASE_URL + "/user/register", payload)
+        .then(function (response) {
+          if (response.status === 200) {
+            setUser((prevState) => ({
+              ...prevState,
+              successMessage:
+                "Registration successful. Redirecting to home page..",
+            }));
+            redirectToHome();
+            props.showError(null);
+          } else {
+            props.showError("Some error ocurred");
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      props.showError("Please enter valid username and password");
     }
-  return(
-        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
-            <form>
-                <div className="form-group text-left">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" 
-                       className="form-control" 
-                       id="email" 
-                       aria-describedby="emailHelp" 
-                       placeholder="Enter email" 
-                       value={user.email}
-                       onChange={handleChange}
-                />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" 
-                        className="form-control" 
-                        id="password" 
-                        placeholder="Password"
-                    />
-                </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Confirm Password</label>
-                    <input type="password" 
-                        className="form-control" 
-                        id="password" 
-                        placeholder="Password"
-                        value={user.password}
-                        onChange={handleChange} 
-                    />
-                </div>
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Sign Up
-                </button>
-            </form>
+  };
+  const redirectToHome = () => {
+    props.updateTitle("Home");
+    props.history.push("/home");
+  };
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+    if (user.password === user.confirmPassword) {
+      sendDetailsToServer();
+    } else {
+      props.showError("Passwords do not match");
+    }
+  };
+  return (
+    <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+      <form>
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
         </div>
-    )
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputPassword1">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmitClick}
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
